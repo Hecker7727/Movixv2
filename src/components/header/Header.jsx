597b021b1bc37e6a21ac5817240d55984579ef7e
haresh <120273246+Hecker7727@users.noreply.github.com>
2024-09-26 10:53:12 +0530

    Add files via upload

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,7 +8,7 @@ import { useNavigate, useLocation } from "react-router-dom";
 import "./Header.scss";
 
 import ContentWrapper from "../contentWrapper/ContentWrapper";
-import logo from "../../assets/movix-logo.svg";
+import logo from "../../assets/movix-logo.png";
 
 const Header = () => {
   const [show, setShow] = useState("top");
